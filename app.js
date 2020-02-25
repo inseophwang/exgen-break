8f5d5ed008e12bab75762ec891bb762a6028d19c
In Seop Hwang <inseop@Ins-MacBook-Pro.local>
2020-02-25 10:22:36 -0500

    fixed

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouters = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouters = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'house'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/users', usersRouter);
+app.use('/', indexRouters)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
